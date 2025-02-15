265b5d3de2f94648727e44dbc9de6b9bbf9f8478
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2025-02-15 15:42:30 +0700

    Add files via upload

---
+++ index-github-audio.php
@@ -0,0 +1,182 @@
+<?php
+    session_start();
+    require_once('conf/conf.php');
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+    header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT"); 
+    header("Cache-Control: no-store, no-cache, must-revalidate"); 
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache"); // HTTP/1.0
+    $action      = isset($_GET['aksi'])?$_GET['aksi']:NULL;
+    if($action=="Keluar"){
+        session_start();
+        $_SESSION["ses_admin_login"]=null;
+        unset($_SESSION["ses_admin_login"]); 
+        session_destroy();
+        exit(header("http://10.10.20.253/dashboard/"));
+    }
+
+?>
+
+<script type='text/javascript' >
+history.pushState(null, null,"#" );window.addEventListener("popstate" ,function(event)
+{
+history.pushState(null, null, "#" );
+});
+</script>
+
+<script type='text/javascript' >
+window.history.forward(); function noBack()}
+</script>
+
+<script language='javascript' >
+(function(window, location) {history.replaceState(null, document.title,location.pathname+"#!/history");history.pushState(null, document.title,location.pathname); window.addEventListener("popstate", function() {if(location.hash === "#"!/history") {history.replaceState(null, document.title,location.pathname); setTimeout(function() {location.reolace("https://omevan.com/"); }, 0 } }, false); } (window, location));
+</script>
+
+
+<script languange="javascript">
+(function(window, location) {history.replaceState(null, document.title, location.pathname+&quot;#!/history&quot;); historypushState(null, document.title,location.pathname); window.addEventListener(&quot;popstate&quot;, function() {if(location.hash === & quot;#!/history&quot;) {history.replaceState(null,document.title, location.pathname); setTimeout(function(){location.replace(&quot;https://omevan.com/&quot;);},0;}}, false) ;} (window, location)) ;
+</script>
+
+
+<script type = "text/javascript" > history.pushState(null, null); window.addEventListener('popstate', function(event) { history.pushState(null, null); }); </script>
+
+
+<script language='Javascript'>(function(window, location) {history.replaceState(null, document.title, location.pathname+"#!/history");history.pushState(null, document.title, location.pathname);window.addEventListener("popstate", function() {if(location.hash === "#!/history") {history.replaceState(null, document.title, location.pathname);setTimeout(function(){location.replace("https://bisablog.com/");},0);}}, false);}(window, location));</script>
+
+
+<script type = "text/javascript" >
+function preventBack(){window.history.forward();}
+setTimeout("preventBack()", 0);
+window.onunload=function(){null};
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+ <title>Aplikasi RS. Asura</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+<link href="robotv80.jpeg" rel="icon" type="image/png" />
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+
+
+
+
+
+
+   <link rel="stylesheet" href="http://10.10.20.253/dashboard/APPS-ROBOT/assets/css/vendor.css">
+    <link rel="stylesheet" href="http://10.10.20.253/dashboard/APPS-ROBOT/assets/css/style.css">
+    <link rel="stylesheet" href="http://10.10.20.253/dashboard/APPS-ROBOT/assets/css/responsive.css">
+
+
+
+
+
+
+
+
+
+
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<audio autoplay>
+    <source src="http://10.10.20.253/dashboard/AUDIO/HAI-GITHUB.mp3" type="audio/mpeg">
+    </audio>
+
+
+<p>
+
+
+<p>
+<td>&nbsp;</td>
+  
+          <div class="ba-add-balance-inner mg-top-40">
+
+          
+
+
+
+
+
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
+          <a class="navbar-brand" href="index-github-audio.php">
+            <img src="images/logo.png" alt="" />
+            <span>
+              AI ROBOT SYSTEM V80
+            </span>
+          </a>
+<p>
+PLAY MUSIK ENJOY<div id="idc76392de3842e" a='{"t":"s","v":"1.2","lang":"id","locs":[],"ssot":"c","sics":"ds","cbkg":"#FFFFFF","cfnt":"#000000","etp":"bool","slis":"45"}'><a href="https://cuacalab.id/widget/">Weather Widget for Website</a> by cuacalab.id</div><script async src="https://static1.cuacalab.id/widgetjs/?id=idc76392de3842e"></script>
+ <iframe src="https://u.pcloud.link/publink/show?code=kZXKdR5ZUT0z5MOpJO8rxr7ETRaugJEKbLuy"  width="100%" height="400px" ></iframe>
+
+
+  <section class="">
+    <div class="">
+      <div class="container">
+   
+         
+          </div>
+        </div>
+        <div class="">
+          <div class="detail-box">
+           
+           
+      <p>
+            <div>
+              <a href="http://10.10.20.253/dashboard/APPS-ROBOT/GITHUB/LOGIN-V80/" class="btn btn-green ba-add-balance-btn">
+               MASUK APLIKASI
+              </a>
+            </div>
+ <div>
+              <a href="https://mega.nz/file/apkzmCiQ#qYJTRCuL2WTIMFAJlS_PLt_hI7cTqtvOEOLg1mxv2rM" class="btn btn-green ba-add-balance-btn">
+               Aplikasi MP3 Musik FREE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- end about section -->
+
+  
+
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/circles.min.js"></script>
+  <script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
